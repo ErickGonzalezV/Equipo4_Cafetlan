d83206eb3c6fa048a8d65676f69c1da563d2154d
Harold Iván GR <ivan092704@gmail.com>
2025-04-24 16:23:10 -0600

    se agrega la funcion de navbar y footer en js

---
+++ js/main.js
@@ -0,0 +1,69 @@
+const navbar = document.getElementById("navbar")
+const footer = document.getElementById("footer")
+
+    navbar.insertAdjacentHTML("afterend",`<nav class="navbar navbar-expand-lg fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="./index.html">Cafetlán</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+
+      <div class="centro d-flex mx-auto">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./nosotros.html">Nosotros</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./productos.html">Productos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./contacto.html">Contáctanos</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="icons d-flex ms-auto ">
+        <ul class="navbar-nav">
+          <li class="nav-link">
+            <i class="bi bi-search" href="#"></i>
+          <li class="nav-link">
+            <i class="bi bi-cart2"></i>
+          </li>
+          <li class="nav-link">
+          <a href="./login.html">
+            <i class="bi bi-person-fill" href="./login.html"></i>
+            </a>
+          </li>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>`);
+
+    footer.insertAdjacentHTML("beforeend", `
+        <footer>
+      <div id="tituloFooter">
+        <h2>Cafetlán</h2>
+      </div>
+      <div id="footerDos">
+          <div class="proyectoFooter">
+            <h3>Proyecto</h3>
+            <h5>Sobre Nosotros</h5>
+            <h5>Tienda</h5>
+            <h5>Únete</h5>
+        </div>
+        <div id="contactoFooter">
+            <h3>Contacto</h3>
+            <h5>Ayuda</h5>
+            <h5>Blog</h5>
+            <h5>Marcas</h5>
+          </div>
+      </div>
+    </footer>  `)
+ 
