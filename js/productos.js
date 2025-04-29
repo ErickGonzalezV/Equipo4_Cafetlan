ff80cffabbdbfec30c9d3007ddc6c9940c40a859
CarooZaav24 <carolineglezav19@gmail.com>
2025-04-29 13:31:27 -0600

    Se agregan los cambios hechos para comenzar a implementar la página de productos

---
+++ js/productos.js
@@ -0,0 +1,30 @@
+
+
+/**CÓDIGO DE LAS CARDS: 
+ <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+ */
+
+/*CÓDIGO DEL COLLAPSE 
+<p>
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+    Toggle width collapse
+  </button>
+</p>
+<div style="min-height: 120px;">
+  <div class="collapse collapse-horizontal" id="collapseWidthExample">
+    <div class="card card-body" style="width: 300px;">
+      This is some placeholder content for a horizontal collapse. It's hidden by default and shown when triggered.
+    </div>
+  </div>
+</div>
+ */
+/*BOTÓN PARA METER DENTRO DEL COLLAPSE 
+<button type="button" class="btn btn-outline-primary">Primary</button>
+*/
