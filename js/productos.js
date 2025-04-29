cac2f18c174022ea93b41bcee40d659b76f9a704
Isaac22 <luisisaacmv22@gmail.com>
2025-04-29 16:33:17 -0600

    comienza a agregarse productos como main

---
+++ js/productos.js
@@ -0,0 +1,152 @@
+function addItem(item){
+    //producto 1
+    const itemHTML = '<div class="card" style="width: 18rem;">\n' +
+        '    <img src="'+"https://res-console.cloudinary.com/dzlbldti5/thumbnails/v1/image/upload/v1745952486/Y2FmZUFydGVzYW5hbFR6dWFjYWxsaV9hd3VhYzY=/drilldown"+'" class="card-img-top" alt="image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">`+Cafe Artesanal T-Zacualli+`</h5>\n' +
+        '        <p class="card-text">'+"Venta de café artesanal tostado en comal de barro con leña, orgánico y de altura originario de Zacualpan de Amilpas Morelos. Combinación de Granos y grano caracolillo."+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+        //producto 2
+        '<div class="card" style="width: 18rem;">\n' +
+        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">'+item.name+'</h5>\n' +
+        '        <p class="card-text">'+item.description+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+                //producto 2
+                '<div class="card" style="width: 18rem;">\n' +
+                '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+                '    <div class="card-body">\n' +
+                '        <h5 class="card-title">'+item.name+'</h5>\n' +
+                '        <p class="card-text">'+item.description+'</p>\n' +
+                '        <a href="#" class="btn btn-primary">Add</a>\n' +
+                '    </div>\n' +
+                '</div>\n' +
+                '<br/>';
+                //producto 4
+                '<div class="card" style="width: 18rem;">\n' +
+                '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+                '    <div class="card-body">\n' +
+                '        <h5 class="card-title">'+item.name+'</h5>\n' +
+                '        <p class="card-text">'+item.description+'</p>\n' +
+                '        <a href="#" class="btn btn-primary">Add</a>\n' +
+                '    </div>\n' +
+                '</div>\n' +
+                '<br/>';
+                //producto 5
+                '<div class="card" style="width: 18rem;">\n' +
+                '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+                '    <div class="card-body">\n' +
+                '        <h5 class="card-title">'+item.name+'</h5>\n' +
+                '        <p class="card-text">'+item.description+'</p>\n' +
+                '        <a href="#" class="btn btn-primary">Add</a>\n' +
+                '    </div>\n' +
+                '</div>\n' +
+                '<br/>';
+                        //producto 6
+        '<div class="card" style="width: 18rem;">\n' +
+        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">'+item.name+'</h5>\n' +
+        '        <p class="card-text">'+item.description+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+                //producto 7
+                '<div class="card" style="width: 18rem;">\n' +
+                '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+                '    <div class="card-body">\n' +
+                '        <h5 class="card-title">'+item.name+'</h5>\n' +
+                '        <p class="card-text">'+item.description+'</p>\n' +
+                '        <a href="#" class="btn btn-primary">Add</a>\n' +
+                '    </div>\n' +
+                '</div>\n' +
+                '<br/>';
+                        //producto 8
+        '<div class="card" style="width: 18rem;">\n' +
+        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">'+item.name+'</h5>\n' +
+        '        <p class="card-text">'+item.description+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+        //producto 9
+        '<div class="card" style="width: 18rem;">\n' +
+        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">'+item.name+'</h5>\n' +
+        '        <p class="card-text">'+item.description+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+                //producto 10
+                '<div class="card" style="width: 18rem;">\n' +
+                '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+                '    <div class="card-body">\n' +
+                '        <h5 class="card-title">'+item.name+'</h5>\n' +
+                '        <p class="card-text">'+item.description+'</p>\n' +
+                '        <a href="#" class="btn btn-primary">Add</a>\n' +
+                '    </div>\n' +
+                '</div>\n' +
+                '<br/>';
+                        //producto 11
+        '<div class="card" style="width: 18rem;">\n' +
+        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">'+item.name+'</h5>\n' +
+        '        <p class="card-text">'+item.description+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+                //producto 12
+                '<div class="card" style="width: 18rem;">\n' +
+                '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+                '    <div class="card-body">\n' +
+                '        <h5 class="card-title">'+item.name+'</h5>\n' +
+                '        <p class="card-text">'+item.description+'</p>\n' +
+                '        <a href="#" class="btn btn-primary">Add</a>\n' +
+                '    </div>\n' +
+                '</div>\n' +
+                '<br/>';
+                        //producto 13
+        '<div class="card" style="width: 18rem;">\n' +
+        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">'+item.name+'</h5>\n' +
+        '        <p class="card-text">'+item.description+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+                //producto 14
+                '<div class="card" style="width: 18rem;">\n' +
+                '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+                '    <div class="card-body">\n' +
+                '        <h5 class="card-title">'+item.name+'</h5>\n' +
+                '        <p class="card-text">'+item.description+'</p>\n' +
+                '        <a href="#" class="btn btn-primary">Add</a>\n' +
+                '    </div>\n' +
+                '</div>\n' +
+                '<br/>';
+    const itemsContainer = document.getElementById("list-items");
+    itemsContainer.innerHTML += itemHTML;
+}
+
+addItem({'name':'juice',
+    'img':'https://www.gs1india.org/media/Juice_pack.jpg',
+    'description':'Orange and Apple juice fresh and delicious'});
+
+addItem({'name':'Tayto',
+    'img':'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg',
+    'description':'Cheese & Onion Chips'})
